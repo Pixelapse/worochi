3cb4edd43f6dcbcf5615119a8b88831923afdd64
Raven Jiang <raven@cs.stanford.edu>
2013-08-14 16:24:53 -0700

    Added bucket name customization for s3_url

--- lib/worochi/helper.rb
+++ lib/worochi/helper.rb
@@ -1,16 +1,36 @@
+require 'awesome_print'
+
 class Worochi
   # Contains any global helper methods not specific to any individual service.
   module Helper
     class << self
       # Given an S3 path, return the full URL for the corresponding object
-      # determined using the AWS SDK.
+      # determined using the AWS SDK. AWS_SECRET_ACCESS_KEY and
+      # AWS_ACCESS_KEY_ID should be present in ENV. The string should be
+      # formatted as: `s3:(bucket_name:)path/to/file`.
       #
-      # @param path [String]
-      # @return [URI::HTTP]
+      # @param path [String] S3 path
+      # @return [URI::HTTPS] URI of the file on S3
+      # @example Pre-configured bucket name
+      #   Worochi::Config.s3_bucket = 'worochi'
+      #   Worochi::Helper.s3_url('s3:test/path')
+      #   # => #<URI::HTTPS URL:https://worochi.s3.amazonaws.com/test/path?AWSAccessKeyId=...>
+      # @example Custom bucket name
+      #   Worochi::Helper.s3_url('s3:bucket-name:a/b')
+      #   # => #<URI::HTTPS URL:https://bucket-name.s3.amazonaws.com/a/b?AWSAccessKeyId=...>
+      # @example Invalid syntax
+      #   Worochi::Helper.s3_url('www.a.com/b.txt')
+      #   # => Worochi::Error
       def s3_url(path)
-        raise Error, 'S3 bucket name is not defined' if Config.s3_bucket.nil?
+        raise Error, 'Invalid S3 path' unless is_s3_path?(path)
         path = path.sub(s3_prefix_re, '')
-        AWS::S3.new.buckets[Config.s3_bucket].objects[path].url_for(:read)
+        if match = /^(.*)\:/.match(path)
+          bucket = match[1]
+          path = path.sub(/^(.*)\:/, '')
+        end
+        bucket ||= Config.s3_bucket
+        raise Error, 'S3 bucket name is not defined' if bucket.nil?
+        AWS::S3.new.buckets[bucket].objects[path].url_for(:read)
       end
 
       # Check if a given path is an S3 path.
--- spec/worochi/helper_spec.rb
+++ spec/worochi/helper_spec.rb
@@ -16,8 +16,20 @@ describe Worochi::Helper do
 
   describe '.s3_url', :aws  do
     it 'returns the right S3 URL' do
-      url = Worochi::Helper.s3_url('s3:test/path')
-      expect(url.to_s).to match(/^https.+s3\.amazonaws\.com\/test\/path.*/)
+      Worochi::Config.s3_bucket = 'rspec-test'
+      uri = Worochi::Helper.s3_url('s3:test/path')
+      expect(uri.path).to eq('/test/path')
+      expect(uri.host).to include('rspec-test')
+    end
+
+    it 'parses bucket names' do
+      uri = Worochi::Helper.s3_url('s3:bucket-name:test/path')
+      expect(uri.path).to eq('/test/path')
+      expect(uri.host).to include('bucket-name')
+    end
+
+    it 'rejects invalid URLs' do
+      expect{Worochi::Helper.s3_url('a/b')}.to raise_error(Worochi::Error)
     end
   end
 end
